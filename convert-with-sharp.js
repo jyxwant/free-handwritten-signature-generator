1cb1fc67e0da7c1085da96e0936f840da5fbdc2e
jyx <jyx1132346005@outlook.com>
2026-02-24 19:57:10 +0800

    Add blog post: How to Create Professional Email Signature

--- convert-with-sharp.js
+++ convert-with-sharp.js
@@ -1,8 +1,8 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const inputPath = 'blog/Generated Image February 21, 2026 - 7_57PM.jpeg';
-const outputPath = 'blog/digital-signature-on-phone-guide.webp';
+const inputPath = 'blog/Generated Image February 24, 2026 - 7_54PM.jpg.jpeg';
+const outputPath = 'blog/email-signature-guide.webp';
 
 sharp(inputPath)
   .webp({ quality: 85 })
