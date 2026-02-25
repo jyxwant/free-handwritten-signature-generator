8c26a6e569a2294586f2e2b92b303f0189cc6c6a
jyxwant <1132346005@qq.com>
2026-02-25 12:42:17 +0800

    Add cursive signature generator blog post, update blog listing and sitemap

--- convert-with-sharp.js
+++ convert-with-sharp.js
@@ -1,8 +1,8 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const inputPath = 'blog/Generated Image February 24, 2026 - 7_54PM.jpg.jpeg';
-const outputPath = 'blog/email-signature-guide.webp';
+const inputPath = 'blog/Generated Image February 25, 2026 - 12_34PM.jpg.jpeg';
+const outputPath = 'blog/cursive-signature-guide.webp';
 
 sharp(inputPath)
   .webp({ quality: 85 })
