5125490489929962afc6d890286cae2444552827
jyxwant <1132346005@qq.com>
2026-02-28 12:27:19 +0800

    Add blog: How to Draw Signature Online Free + update sitemap & blog listing

--- convert-with-sharp.js
+++ convert-with-sharp.js
@@ -1,8 +1,8 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const inputPath = 'blog/Generated Image February 27, 2026 - 9_33PM.png';
-const outputPath = 'blog/digital-signature-vs-electronic-signature.webp';
+const inputPath = 'blog/Generated Image February 28, 2026 - 12_23PM.jpg.jpeg';
+const outputPath = 'blog/draw-signature-online-guide.webp';
 
 sharp(inputPath)
   .webp({ quality: 85 })
